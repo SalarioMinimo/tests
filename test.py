4843a63244b36ccfda030a7bf614cf37938b74d6
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-11 19:30:52 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import sympy
 
-x = sympy.symbols("x")
-
-expresion = (x**2 +20)
-sympy.preview(expresion,viewer="file",filename="uwu.png")
+from sympy.utilities.misc import find_executable
+find_executable('latex')
