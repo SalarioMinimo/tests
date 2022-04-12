90ba961bd8161feff0178edae792bc2a8687d405
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-11 19:59:54 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def uwu():
   st.title("nos mataran a todos")
-input=st.text_area(label="Text in latex",on_change=uwu())
+input=st.text_area(label="Text in latex",on_change=uwu)
 st.latex(input)
