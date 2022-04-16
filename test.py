19f8a022b559a2ac8b6f4e5fb2edf9a44766ef5f
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-16 15:17:20 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import streamlit as st
 from sympy import *
 from nltk import word_tokenize
 
+esta = word_tokenize({"vamos a matar unos perros."})
+
+esta
 x = symbols("x")
 exp = sqrt(6 / 2) + 8 - x
 st.latex(exp)
