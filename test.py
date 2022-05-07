91f8f1f231b6249baaad69630e8040f8861fc7a2
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 01:06:23 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,22 @@ import streamlit as st
 
 class side_bar:
   
+  
+
+  
   def __init__(self):
     with st.sidebar:
       st.title("Documentación")
       st.subheader("Todo lo que necesitas saber")
+      
+      
+      l1, col2, col3 = st.columns([1,1,1])
+
+      with col1:
+        st.button('1')
+      with col2:
+        st.button('2')
+      with col3:
+        st.button('3')
       
 Documentacion = side_bar()
