bbf7a0496c775baa9fc7fcb8760caa46fe425063
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 01:34:45 -0600

    Update test.py

--- test.py
+++ test.py
@@ -10,15 +10,8 @@ class side_bar:
       st.title("Manual de uso")
       st.subheader("Bienvenido a mi calculadora, aquí está toda la información que debes de saber para usarla.")
       
-      
-      col1, col2, col3 = st.columns(3)
+      genre = st.radio("Selecciona un apartado",("Justificacion", "Comandos", "Ejemplos"))
 
-      with col1:
-        st.button(label = "¿Por qué una calculadora de texto?", on_click = self.comandos)
-      with col2:
-        st.button("¿Qué puedo llegar a hacer?")
-      with col3:
-        st.button("¿Cómo puedo usarla?")
         
   def comandos(self):
       st.sidebar.button("Funcionará¿")
