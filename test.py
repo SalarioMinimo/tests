788bd0bb8739a8213e30a035435fd06679352830
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 01:39:43 -0600

    Update test.py

--- test.py
+++ test.py
@@ -7,13 +7,21 @@ class side_bar:
   
   def __init__(self):
     with st.sidebar:
+      selection = {"Justificación":self.Justificación,"Comandos":self.Comandos,"Ejemplos":self.Ejemplos}
       st.title("Manual de uso")
       st.subheader("Bienvenido a mi calculadora, aquí está toda la información que debes de saber para usarla.")
       
       genre = st.radio("Selecciona un apartado",("Justificacion", "Comandos", "Ejemplos"))
 
-        
-  def comandos(self):
-      st.sidebar.button("Funcionará¿")
+  def Justificación(self):
+    st.sidebar.text("Quizás funciona")
+    
+  def Comandos(self):
+    st.sidebar.text("Funcionará¿")
+    
+  def Ejemplos(self):
+    st.sidebar.text("Esta funcionando")
+      
+      
       
 Documentacion = side_bar()
