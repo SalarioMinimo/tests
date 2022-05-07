b0b5c7a45023e8359a58877ab4dbd1b8a8aa1106
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 01:09:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,10 @@ class side_bar:
       col1, col2, col3 = st.columns([1,1,1])
 
       with col1:
-        st.button('1')
+        st.button("¿Por qué una calculadora de texto?")
       with col2:
-        st.button('2')
+        st.button("¿Qué puedo llegar a hacer?")
       with col3:
-        st.button('3')
+        st.button("¿Cómo puedo usarla?")
       
 Documentacion = side_bar()
