2a0b17c3f49ca729daf0a4e716b52764e0f6eb88
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 00:48:13 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
 import streamlit as st
-from sympy import *
-import nltk
 
-nltk.download('punkt')
-esta = nltk.word_tokenize("(64) entre seno de 23,")
-
-esta
-x = symbols("x")
-exp = sqrt(6 / 2) + 8 - x
-st.latex(exp)
-
-
-st.title(solve(exp,x))
+with st.sidebar:
+  st.text("uwu")
